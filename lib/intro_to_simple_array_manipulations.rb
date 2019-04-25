37afbd59a9161cbd0c9abedc29f22f65c58940e6
Maija Barrins <maija.barrins@gmail.com>
2019-04-25 20:37:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,31 @@
-def using_push(array, string)
-  array.push(string)
+def using_push(array, element)
+  array.push(element)
 end
 
-def using_unshift(array, string)
-  array.unshift(string)
+def using_unshift(array, element)
+  array.unshift(element)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array, number)
+  array.pop(number)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array, number)
+  array.shift(number)
+end
+
+def using_concat(array1, array2)
+
+end
+
+def using_insert(array, element)
+  
 end
